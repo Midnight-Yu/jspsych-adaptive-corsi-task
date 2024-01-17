bef785565daa3dfaab4845c992410215c94e5791
Midnight-Yu <156907600+Midnight-Yu@users.noreply.github.com>
2024-01-17 23:58:01 +0800

    Add files via upload

--- scripts/exp.js
+++ scripts/exp.js
@@ -1,29 +1,32 @@
 /* 全局参数初始化 */
 // 方块布局
-var test_block_arrangement = [ 
-    {x:30, y:30}, {x:40, y:30}, {x:50, y:30}, {x:60, y:30}, {x:70, y:30},
-    {x:30, y:40}, {x:40, y:40}, {x:50, y:40}, {x:60, y:40}, {x:70, y:40},
-    {x:30, y:50}, {x:40, y:50}, {x:50, y:50}, {x:60, y:50}, {x:70, y:50},
-    {x:30, y:60}, {x:40, y:60}, {x:50, y:60}, {x:60, y:60}, {x:70, y:60},
-    {x:30, y:70}, {x:40, y:70}, {x:50, y:70}, {x:60, y:70}, {x:70, y:70},
+var test_block_arrangement = [
+    { x: 30, y: 30 }, { x: 40, y: 30 }, { x: 50, y: 30 }, { x: 60, y: 30 }, { x: 70, y: 30 },
+    { x: 30, y: 40 }, { x: 40, y: 40 }, { x: 50, y: 40 }, { x: 60, y: 40 }, { x: 70, y: 40 },
+    { x: 30, y: 50 }, { x: 40, y: 50 }, { x: 50, y: 50 }, { x: 60, y: 50 }, { x: 70, y: 50 },
+    { x: 30, y: 60 }, { x: 40, y: 60 }, { x: 50, y: 60 }, { x: 60, y: 60 }, { x: 70, y: 60 },
+    { x: 30, y: 70 }, { x: 40, y: 70 }, { x: 50, y: 70 }, { x: 60, y: 70 }, { x: 70, y: 70 },
 ];
 
 // 方块ID列表，用在随机化函数里面
 var block_id_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 
 // 方块大小
-var block_size = 9.5; 
+var block_size = 9.5;
 
 // 测试区域长宽（有可能做针对屏幕的修改吗？（或许并不需要））
-var display_width = "800px"; 
-var display_height = "800px"; 
+var display_width = "800px";
+var display_height = "800px";
+
+// 测试用，外部储存一个用来传sequence的全局变量（万一成了呢）（这做法不太对啊）
+local_sequence = [];
 
 // 随机抽取算法
 function getRandomElements(array, x) {
     const newArray = array
     const shuffledArray = newArray.sort(() => Math.random() - 0.5);
     return shuffledArray.slice(0, x);
-  }
+}
 
 /* 初始化jsPsych，注意添加的实验强制退出 */
 let jsPsych = initJsPsych({
@@ -37,7 +40,7 @@ let jsPsych = initJsPsych({
             .get()
             .localSave('csv', 'data.csv')
     }
-}); 
+});
 
 // 注册一个listener，用于实验强制退出
 function endExperiment(e) {
@@ -56,30 +59,42 @@ let instruction = {
     </div>
     `,
     post_trial_gap: 500
-}
+};
 
-let show_sequence = {
-    type: jsPsychCorsiBlocks,
-    blocks: test_block_arrangement,
-    block_size : block_size,
-    display_height : display_height,
-    display_width : display_width,
-    sequence: getRandomElements(block_id_list, 3),
-    mode: 'display'
-}
-
-let response = {
-    type: jsPsychCorsiBlocks,
-    blocks: test_block_arrangement,
-    block_size : block_size,
-    display_height : display_height,
-    display_width : display_width,
-    sequence: function() {
-        original_list = show_sequence.sequence;
-        reversed_list = original_list.reverse();
-        return reversed_list;
-    }, //欸我是不是可以在这里就给它倒序了 //如果要让它成为一个动态参数的话，需要写一个function，不然这里只初始化一次
-    mode: 'input'
+let example_timeline = {
+    timeline: [
+        {
+            type: jsPsychCorsiBlocks,
+            blocks: test_block_arrangement,
+            block_size: block_size,
+            display_height: display_height,
+            display_width: display_width,
+            sequence: function () {
+                randomized_array = getRandomElements(block_id_list, 3);
+                local_sequence = randomized_array;
+                return randomized_array;
+            },
+            mode: 'display'
+        },
+        {
+            type: jsPsychCorsiBlocks,
+            blocks: test_block_arrangement,
+            block_size: block_size,
+            display_height: display_height,
+            display_width: display_width,
+            sequence: function () {
+             // original_list = example_timeline.timeline[0].sequence; //写成这样有问题，获取到的是上面的函数了，不是数值，这里要传参了//需要获取timeline里上一个trial的参数
+                original_list = local_sequence;
+                reversed_list = original_list.reverse();
+                return reversed_list;
+            }, 
+            mode: 'input'
+        }
+    ],
+    sample: {
+        type: 'fixed-repetitions',
+        size: 3
+    }
 }
 
 let ending = {
@@ -88,10 +103,10 @@ let ending = {
     <div class='experiment-instruction'><p>实验已结束</p></div>
     `,
     post_trial_gap: 500
-}
+};
 
 /* jsPsych 运行*/
 jsPsych.run([
-    instruction, show_sequence, response, ending
-])
+    instruction, example_timeline, ending
+]);
 
